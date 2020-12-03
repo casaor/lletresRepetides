a9a8f716d1473c3522c4e8c3a3c4c9375b8496c6
carlota sas <casaor@outlook.com>
2020-12-03 17:25:19 +0100

    fase 2

---
+++ Fase 2/lletres.js
@@ -0,0 +1,25 @@
+let lletres = ["C","A","R","L","0","T","A"];
+const vocals = ["A","E","I","O","U"];
+let i;
+let j;
+
+console.log("Partim del nom: CARL0TA");
+
+lletres.map(function(lletra){
+    if (!isNaN(lletra)) {
+        console.log("Els noms de persones no contenen el número: " + lletra); 
+    }else{
+        let trobat = false;
+        vocals.map(function(vocal){
+            if(lletra == vocal){
+                trobat= true;
+            }
+        })
+
+        if (trobat){
+            console.log("He trobat la VOCAL: " + lletra);
+        }else{
+            console.log("He trobat la CONSONANT: " + lletra);
+        }
+    }
+})
